e3a3bc5c8be9beeff9afc25f3b7117584edebd55
will-mcn <wam59@georgetown.edu>
2022-03-28 18:14:56 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,7 @@ def to_usd(my_price):
     Invoke like this: to_usd(0.000)
     """
     return '${:,.2f}'.format(my_price)
+
+if __name__ == "__main__":
+    price = input("Please choose a price like 10.999:")
+    print(to_usd(float(price)))
